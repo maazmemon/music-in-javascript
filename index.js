5cf1fc830a0b5d4e17863f546f8d76e94837e834
maazmemon <mmemon4151@gmail.com>
2021-10-30 22:39:42 +0530

    commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var MAAZ = document.querySelectorAll(".drum").length;
+
+for ( var i=0; i<=MAAZ ; i++){
+
+document.querySelectorAll("button")[i].addEventListener("click", function (){
+    alert ("umm nice") } );
+}    
